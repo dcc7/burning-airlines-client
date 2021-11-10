5652f8f99abe95f8d6feed023e97905849a2f0ea
Daniel <danielchoe7@gmail.com>
2021-11-11 09:52:19 +1100

    final

--- src/pages/Flights.js
+++ src/pages/Flights.js
@@ -50,7 +50,7 @@ class Search extends Component {
         <h1>Seating</h1>
         <h4>Plane Name: { this.state.name }</h4>
         <h1>{ this.state.seats } Seats Available</h1>
-        <SeatsMap seats={ this.state.seats }/>
+        <SeatsMap seatrows={ this.state.rows } seatcolumns={ this.state.columns }/>
       </div>
     );
   }
@@ -59,24 +59,32 @@ class Search extends Component {
 export default Search;
 
 
+
+
+
 // Seats map component. Placed here out of laziness.
-
 const SeatsMap = (props) => {
-  const seatArray = Array.from(Array(props.seats).keys());
-  const seats = seatArray.map((seat) => <button key={ seat }>{ seat + 1 }</button>)
-
-
-  // for (let i = 0; i <= props.columns; i++) {
-  //   for (let j = 0; j <= props.rows; j++ ) {
-  //     <button>Seat</button>
-  //   }
-  // }
+  // const seatArray = Array.from(Array(props.seats).keys());
+  // const seats = seatArray.map((seat) => <button key={ seat }>s{ seat + 1 }</button>)
 
 
   return (
+    // for (let j = 0; j <= props.rows; j++ ) {
+    //
+    //
+    // }
+
     <div>
       <h3>Seat Number</h3>
-      { seats }
+      <table>
+        { for (let i = 0; i < props.rows; i++ ) => {
+          <tr id=i >
+          for (let j = 0; j < props.columns; j++ ) => {
+            <td id=j >ij</td>
+            }
+          </tr>
+        } }
+      </table>
     </div>
   )
 };
